b2120ae872f8553271e6065bd09210a9ea5a914c
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2019-04-13 15:49:36 -0400

    add structure of Nginx/Flask/docker stack with hello world endpoints

---
+++ app/synctool.py
@@ -0,0 +1,43 @@
+import logging
+import json
+from flask import Flask, render_template, request, abort, send_file
+
+app = Flask(__name__)
+#CORS(app, resources=r'/api/*')		# Allow any origin for requests to paths starting with /api/
+
+"""Logging setup"""
+# create logger
+logger = logging.getLogger('com.directenergypartners.simserver')
+logger.setLevel(logging.DEBUG)
+
+# create console handler 
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(ch)
+
+if __name__ != '__main__':	# only do this when running w gunicorn
+	glogs = logging.getLogger('gunicorn.error')
+	# don't need to put time in logs because it's already there
+	formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+	
+	ch.setFormatter(formatter)
+	for h in glogs.handlers:
+		h.setFormatter(formatter)
+		#logger.setLevel(logging.DEBUG, h)
+		logger.addHandler(h)
+		
+
+""" Endpoints """
+@app.route('/api/hello-world', methods=['GET'])
+def create_system():
+	logger.debug('Got a GET request to /api/hello-world')
+	
+	return("Hello from Flask!")
+	
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
