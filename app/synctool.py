1c74abe3b9473d003f9a97d15788f5d146f1d29f
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2019-04-19 17:00:53 -0400

    forgot to import glitchlab_shopify lol

--- app/synctool.py
+++ app/synctool.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, abort, send_from_directory, s
 import os
 import requests
 import datetime
+import glitchlab_shopify
 from flask_cors import CORS
 
 """Debug URLLib requests"""
