dd231b78fffff1967e5f91b983fdf92b19d98ec2
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2019-04-19 20:42:13 -0400

    ebay does not give back a new refresh token

--- app/synctool.py
+++ app/synctool.py
@@ -270,7 +270,6 @@ def refresh_access_token(refresh_token):
 	# Set session params
 	session['access_token'] = authDict['access_token']
 	session['refresh_token_expiry'] = datetime.datetime.utcnow() + datetime.timedelta(seconds=authDict['expires_in'])
-	session['refresh_token'] = authDict['refresh_token']
 	
 	return authDict
 	
