cbc99e4216e968039e3afceaf38b528a00332f4f
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2019-06-05 23:19:40 -0400

    no message

--- app/synctool.py
+++ app/synctool.py
@@ -83,6 +83,7 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
                 attach_to_all=True, automatic_options=True):
     import datetime
     from functools import update_wrapper
+    from flask import make_response, request, current_app
     """Decorator function that allows crossdomain requests.
       Courtesy of
       https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
