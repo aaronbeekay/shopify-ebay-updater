e5d9dc33064c65fcb5fe8f45aec6ede8a50e55b4
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2019-06-14 20:54:26 -0400

    delete offers before writing ebay product

--- app/synctool.py
+++ app/synctool.py
@@ -386,6 +386,7 @@ def ebay_product_endpoint():
 					glitchlab_shopify.update_ebay_offer( offer['offerId'], offer )
 		
 			# Now update the product too
+			del new['offers']
 			logger.debug( "Updating eBay SKU {} - got new attributes {}".format(request.args.get('sku'), new))
 			glitchlab_shopify.set_ebay_attributes( request.args.get('sku'), new )
 			
